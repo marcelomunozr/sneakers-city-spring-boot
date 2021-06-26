30bba98f9f8a69f992dcfe5ebb67fd2c9778dbab
Marcelo Enrique Munoz Rodriguez <memunozro.2brains@bancochile.cl>
2021-06-26 01:32:06 -0400

    mejoras controller

--- src/main/java/com/seneakers/springbootapp/controller/SneakerController.java
+++ src/main/java/com/seneakers/springbootapp/controller/SneakerController.java
@@ -13,28 +13,28 @@ import java.util.Map;
 @RestController
 @RequestMapping("api/sneakers")
 public class SneakerController {
-	
+
 	@Autowired
-    private SneakerService sneakerService;
+	private SneakerService sneakerService;
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Sneaker saveSneaker(@RequestBody Sneaker sneaker) {
-        return sneakerService.create(sneaker);
-    }
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Sneaker saveSneaker(@RequestBody Sneaker sneaker) {
+		return sneakerService.create(sneaker);
+	}
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Sneaker> getAllSneakers() {
-        return sneakerService.read();
-    }
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Sneaker> getAllSneakers() {
+		return sneakerService.read();
+	}
 
-    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Sneaker updateSneaker(@RequestBody Sneaker sneaker) {
-        return sneakerService.update(sneaker);
-    }
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Sneaker updateSneaker(@RequestBody Sneaker sneaker) {
+		return sneakerService.update(sneaker);
+	}
 
-    @DeleteMapping("/{id}")
-    public Map<String, String> deleteSneaker(@PathVariable String id) {
+	@DeleteMapping("/{id}")
+	public Map<String, String> deleteSneaker(@PathVariable String id) {
 
-        return sneakerService.delete(id);
-    }
+		return sneakerService.delete(id);
+	}
 }
